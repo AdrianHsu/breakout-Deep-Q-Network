dfa3bcb07f5fb9cdc2add55757e434020da89805
AdrianHsu <adrianhsu1995@gmail.com>
2018-06-12 20:51:53 +0800

    maze from mofan tutorial

---
+++ maze/run_this.py
@@ -0,0 +1,42 @@
+"""
+Reinforcement learning maze example.
+
+Red rectangle:          explorer.
+Black rectangles:       hells       [reward = -1].
+Yellow bin circle:      paradise    [reward = +1].
+All other states:       ground      [reward = 0].
+
+This script is the main part which controls the update method of this example.
+The RL is in RL_brain.py.
+
+View more on my tutorial page: https://morvanzhou.github.io/tutorials/
+"""
+
+from maze_env import Maze
+from RL_brain import QLearningTable
+
+
+def update():
+    
+    for episode in range(100):
+        obs = env.reset()
+
+        while True:
+            env.render()
+            act = RL.choose_action(str(obs))
+            obs_, reward, done = env.step(act)
+            RL.learn(str(obs), act, reward, str(obs_))
+            obs = obs_
+            if done:
+                break
+
+    # end of game
+    print('game over')
+    env.destroy()
+
+if __name__ == "__main__":
+    env = Maze()
+    RL = QLearningTable(actions=list(range(env.n_actions)))
+
+    env.after(100, update)
+    env.mainloop()
