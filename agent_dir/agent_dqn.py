da8f1ec5a22eeadab1e98953e438459e1998bd40
AdrianHsu <adrianhsu1995@gmail.com>
2018-06-16 22:12:36 +0800

    first version done

--- agent_dir/agent_dqn.py
+++ agent_dir/agent_dqn.py
@@ -1,7 +1,17 @@
 from agent_dir.agent import Agent
+from colors import *
+from collections import deque
+
 import tensorflow as tf
 import numpy as np
 import os
+import random
+
+random.seed(0)
+np.random.seed(0)
+tf.set_random_seed(0)
+gpu_config = tf.ConfigProto()
+gpu_config.gpu_options.allow_growth = True
 
 class Agent_DQN(Agent):
   def __init__(self, env, args):
@@ -12,7 +22,11 @@ class Agent_DQN(Agent):
 
     super(Agent_DQN,self).__init__(env)
     self.args = args
+    self.batch_size = args.batch_size
+    self.lr = args.learning_rate
+    self.gamma = args.gamma_reward_decay
     self.n_actions = env.action_space.n # = 4
+    self.state_dim = env.observation_space.shape[0] # 84
     self.step = 0
 
     if args.test_dqn:
@@ -21,10 +35,31 @@ class Agent_DQN(Agent):
 
     self.s = tf.placeholder(tf.float32, [None, 84, 84, 4], 
       name='s')
-    self.q_target = tf.placeholder(tf.float32, [None, self.n_actions], name='Q_target')
+    self.s_ = tf.placeholder(tf.float32, [None, 84, 84, 4], 
+      name='s_')
+    self.y_input = tf.placeholder(tf.float32, [None]) 
+    self.action_input = tf.placeholder(tf.float32, [None, self.n_actions])
 
-    self.build_net('eval_net')
-    # self.build_net('target_net')
+    self.q_eval = self.build_net(self.s, 'eval_net')
+    self.q_target = self.build_net(self.s_, 'target_net')
+    self.buildOptimizer()
+    t_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='target_net')
+    e_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='eval_net')
+
+    self.replace_target_op = [tf.assign(t, e) for t, e in zip(t_params, e_params)] 
+
+    self.epsilon = self.args.epsilon_start
+    self.replay_memory = deque()
+
+    self.sess = tf.Session(config=gpu_config)
+
+    if tf.gfile.Exists("logs/"):
+      print('remove logs/')
+      tf.gfile.DeleteRecursively("logs/")
+    tf.gfile.MakeDirs("logs/")
+
+    self.summary_writer = tf.summary.FileWriter("logs/", graph=self.sess.graph)
+    self.sess.run(tf.global_variables_initializer())
 
   def init_game_setting(self):
     """
@@ -36,84 +71,68 @@ class Agent_DQN(Agent):
     ##################
     pass
 
-  def build_net(self, var_scope):
+  def build_net(self, s, var_scope):
 
-    with tf.variable_scope(var_scope):
-      cnames = [var_scope + '_params', tf.GraphKeys.GLOBAL_VARIABLES]
-      
+    with tf.variable_scope(var_scope):      
       with tf.variable_scope('conv1'):
-        W1 = self.init_W(shape=[8, 8, 4, 32], 
-              collections=cnames)
-        b1 = self.init_b(shape=[32],
-              collections=cnames)
-        conv1 = self.conv2d(self.s, W1, strides=4)
+        W1 = self.init_W(shape=[8, 8, 4, 32])
+        b1 = self.init_b(shape=[32])
+        conv1 = self.conv2d(s, W1, strides=4)
         h_conv1 = tf.nn.relu(tf.nn.bias_add(conv1, b1))
 
       # with tf.name_scope('max_pool1'):
       #   h_pool1 = self.max_pool(h_conv1)
 
       with tf.variable_scope('conv2'):
-        W2 = self.init_W(shape=[4, 4, 32, 64],
-              collections=cnames)
-        b2 = self.init_b(shape=[64],
-              collections=cnames)
+        W2 = self.init_W(shape=[4, 4, 32, 64])
+        b2 = self.init_b(shape=[64])
         conv2 = self.conv2d(h_conv1, W2, strides=2)
         h_conv2 = tf.nn.relu(tf.nn.bias_add(conv2, b2))
 
       with tf.variable_scope('conv3'):
-        W3 = self.init_W(shape=[3, 3, 64, 64],
-              collections=cnames)
-        b3 = self.init_b(shape=[64],
-              collections=cnames)
+        W3 = self.init_W(shape=[3, 3, 64, 64])
+        b3 = self.init_b(shape=[64])
         conv3 = self.conv2d(h_conv2, W3, strides=1)
         h_conv3 = tf.nn.relu(tf.nn.bias_add(conv3, b3))
 
       h_flatten = tf.reshape(h_conv3, [-1, 3136])
       
       with tf.variable_scope('fc1'):
-        W_fc1 = self.init_W(shape=[3136, 512],
-                  collections=cnames)
-        b_fc1 = self.init_b(shape=[512],
-                  collections=cnames)
+        W_fc1 = self.init_W(shape=[3136, 512])
+        b_fc1 = self.init_b(shape=[512])
         fc1 = tf.nn.bias_add(tf.matmul(h_flatten, W_fc1), b_fc1)
         h_fc1 = tf.nn.relu(fc1)
 
       with tf.variable_scope('fc2'):
-        W_fc2 = self.init_W(shape=[512, self.n_actions],
-                  collections=cnames)
-        b_fc2 = self.init_b(shape=[self.n_actions],
-                  collections=cnames)
-        fc2 = tf.nn.bias_add(tf.matmul(h_fc1, W_fc2), b_fc2)
-
-      exit(0)
-
-
-
-  def init_W(self, shape, name='weights', collections=None,
-    w_initializer=tf.truncated_normal_initializer(0., 1e-1)):
+        W_fc2 = self.init_W(shape=[512, 4])
+        b_fc2 = self.init_b(shape=[4])
+        fc2 = tf.nn.bias_add(tf.matmul(h_fc1, W_fc2), b_fc2, name='Q')
     
-    return tf.get_variable(
-            name=name,
-            shape=shape, 
-            initializer=w_initializer,
-            collections=collections)
+    return fc2
 
-  def init_b(self, shape, name='biases', collections=None,
+  def init_W(self, shape, name='weights', 
+    w_initializer=tf.truncated_normal_initializer(0, 1e-1)):
+
+    return tf.get_variable(
+      name=name,
+      shape=shape, 
+      initializer=w_initializer)
+
+  def init_b(self, shape, name='biases', 
     b_initializer = tf.constant_initializer(1e-1)):
 
     return tf.get_variable(
-            name=name,
-            shape=shape,
-            initializer=b_initializer,
-            collections=collections)
+      name=name,
+      shape=shape,
+      initializer=b_initializer)
 
   def conv2d(self, x, kernel, strides=4):
 
     return tf.nn.conv2d(
-          input=x, 
-          filter=kernel, 
-          strides=[1, strides, strides, 1], 
-          padding="VALID")
+      input=x, 
+      filter=kernel, 
+      strides=[1, strides, strides, 1], 
+      padding="VALID")
 
   def max_pool(self, x, ksize=2, strides=2):
     return tf.nn.max_pool(x, 
@@ -121,13 +140,22 @@ class Agent_DQN(Agent):
       strides=[1, strides, strides, 1], 
       padding="SAME")
 
-  def storeTransition(self, obs, action, reward, obs_):
+  def buildOptimizer(self):
+    with tf.variable_scope('loss'):
+      q_actions = tf.reduce_sum(tf.multiply(self.q_eval, self.action_input), reduction_indices=1)
+      self.loss = tf.reduce_mean(tf.squared_difference(self.y_input, q_actions, name='td_error'))
+      self.train_summary = tf.summary.scalar('loss', self.loss)
+      tf.summary.merge_all()
+    with tf.variable_scope('train'):
+      self.logits = tf.train.RMSPropOptimizer(self.lr).minimize(self.loss)
+
+  def storeTransition(self, s, action, reward, s_, done):
     """
     Store transition in this step
     Input:
-        obs: np.array
+        s: np.array
             stack 4 last preprocessed frames, shape: (84, 84, 4)
-        obs_: np.array
+        s_: np.array
             stack 4 last preprocessed frames, shape: (84, 84, 4)
         action: int (0, 1, 2, 3)
             the predicted action from trained model
@@ -136,40 +164,92 @@ class Agent_DQN(Agent):
     Return:
         None
     """
+    one_hot_action = np.zeros(self.n_actions)
+    one_hot_action[action] = 1
+    self.replay_memory.append((s, one_hot_action, reward, s_, done))
 
   def learn(self):
-    pass
+    minibatch = random.sample(self.replay_memory, self.batch_size)
+    state_batch = [data[0] for data in minibatch]
+    action_batch = [data[1] for data in minibatch]
+    reward_batch = [data[2] for data in minibatch]
+    next_state_batch = [data[3] for data in minibatch]
+    done_batch = [data[4] for data in minibatch]
+    # print(np.array(state_batch).shape) # (32, 84, 84, 4)
+
+    q_eval_batch = self.sess.run(self.q_target, 
+      feed_dict={self.s_: next_state_batch})
+
+    q_eval_storage = []
+
+    for i in range(self.batch_size):
+      done = done_batch[i]
+      if done:
+        q_eval_storage.append(reward_batch[i])
+      else:
+        q = reward_batch[i] + self.gamma * np.max(q_eval_batch[i])
+        q_eval_storage.append(q)
+
+    run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+    _, summary, loss = self.sess.run([self.logits, self.train_summary, self.loss], feed_dict={
+      self.s: state_batch,
+      self.y_input: q_eval_storage,
+      self.action_input: action_batch
+      }, options=run_options)
+    # print('loss: ', loss);
+
+    if self.step % self.args.update_time == 0:
+      self.summary_writer.add_summary(summary, global_step=self.step)
+      self.sess.run(self.replace_target_op)
+      # print('target_params_replaced')
 
   def train(self):
+
     """
     Implement your training algorithm here
     """
+
     for episode in range(self.args.num_episodes):
-
+      # print('episode: ', episode)
+      # "state" is also known as "observation"
       obs = self.env.reset() #(84, 84, 4)
-      print(episode)
-      while True:
-        # env.render() # our code doesn't support this
-
-        action = self.make_action(obs)
+      rewards = 0
+      for s in range(self.args.max_num_steps):
+        # self.env.env.render()
+        action = self.make_action(obs, test=False)
         obs_, reward, done, info = self.env.step(action)
-        # print(info)
-        self.storeTransition(obs, action, reward, obs_)
-
-        if (self.step > 200) and (self.step % 5 == 0):
+        rewards += reward
+        self.storeTransition(obs, action, reward, obs_, done)
+        self.step += 1
+        if len(self.replay_memory) > self.args.replay_size:
+          self.replay_memory.popleft()
+        # once the storage stored > batch_size, start training
+        if len(self.replay_memory) > self.batch_size:
           self.learn()
         
         obs = obs_
 
         if done:
           break
-        
-        self.step += 1
+
+      if episode % self.args.num_eval == 0:
+        total_reward = 0
+        for i in range(self.args.num_test_episodes):
+          obs = self.env.reset()
+          # self.env.env.render()
+          action = self.make_action(obs, test=True)
+          obs_, reward, done, info = self.env.step(action)
+          total_reward += reward
+          if done:
+            break
+        avg_reward = total_reward / self.args.num_test_episodes
+        print('episode: ', episode, ', eval average reward: ', avg_reward)
+        if avg_reward >= 40.0: # baseline
+          print('baseline passed!')
+          break
 
     print('game over')
     # env.destroy()
-        
-
 
   def make_action(self, observation, test=True):
     """
@@ -181,7 +261,26 @@ class Agent_DQN(Agent):
         action: int
             the predicted action from trained model
             """
-    ##################
-    # YOUR CODE HERE #
-    ##################
-    return self.env.get_random_action()
+    state = observation.reshape((1,84,84,4))
+    q_value = self.sess.run(self.q_eval, feed_dict={self.s: state})[0]
+
+    if test:
+      return np.argmax(q_value)
+
+    if random.random() <= self.epsilon and not test:
+      action = random.randrange(self.n_actions)
+    else:
+      action = np.argmax(q_value)
+
+    if self.epsilon > self.args.epsilon_end \
+        and self.step > self.args.observe_steps:
+      old_e = self.epsilon
+      interval = self.args.epsilon_start - self.args.epsilon_end
+      self.epsilon -= interval / float(self.args.anneal_rate)
+      # print('epsilon: ', old_e, ' -> ', self.epsilon)
+
+    return action
+
+    # return self.env.get_random_action()
+
+
