78b6efe8addcbc40b86148eb90a1fdadff8ef232
AdrianHsu <adrianhsu1995@gmail.com>
2018-06-19 13:10:35 +0800

    not work

--- agent_dir/agent_dqn.py
+++ agent_dir/agent_dqn.py
@@ -229,8 +229,9 @@ class Agent_DQN(Agent):
 
       self.train_summary = tf.summary.merge(self.train_summary)
     with tf.variable_scope('train'):
-      #self.logits = tf.train.RMSPropOptimizer(self.lr).minimize(self.loss)
-      self.logits = tf.train.RMSPropOptimizer(self.lr, decay=0.99, epsilon=1e-6).minimize(self.loss)
+      self.logits = tf.train.RMSPropOptimizer(self.lr).minimize(self.loss)
+      #self.logits = tf.train.AdamOptimizer(self.lr).minimize(self.loss)
+      #self.logits = tf.train.RMSPropOptimizer(self.lr, decay=0.99, epsilon=1e-6).minimize(self.loss)
 
   def storeTransition(self, s, action, reward, s_, done):
     """
